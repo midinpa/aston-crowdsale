2d1bf29f9fa14f76c908c3891128c9267b5f0e5e
midinpa <midinpa@gmail.com>
2017-12-25 17:16:34 +0900

    registerlist processor

--- scripts/main_register_kyc.js
+++ scripts/main_register_kyc.js
@@ -15,12 +15,55 @@ const kycOwnerAccount = "0x266085436a4d9cb25603a3b1ab72338f313d7377";
 
 const kyc = web3.eth.contract(kycAbi).at(kycAddress);
 
-const registerList = require('./main_registerList');
+const rawList = require('./main_registerList');
+const processed_registerList = [];
 
-// EXECUTE
-const register = async() => {
+//PROCESS REGISTERLIST
+const registerlistProcessor = () => {
+  let isError;
+
+  for (let i = 1; i < rawList.split('\n').length - 1; i++) {
+    let element = rawList.split('\n')[i]
+
+    element = element.replace(/(\s*)/g, "");
+
+    if (!element.startsWith('0x')) {
+      element = '0x'.concat(element)
+    }
+
+    if (element.length != 42) {
+      console.log("address length problem : %d th element", i, element)
+      isError = true;
+    }
+    processed_registerList.push(element);
+  }
+  return isError
+}
+
+// CHECK
+const check_registerList = async(_registerList) => {
+  const checkList = []
+  let isError
+  for (let i = 0; i < _registerList.length; i++) {
+    if (await kyc.registeredAddress(_registerList[i])) {
+      console.log(i, _registerList[i], " is already registered before")
+      isError = true;
+    }
+
+    if (checkList.indexOf(_registerList[i]) != -1) {
+      console.log(i, _registerList[i], " is duplicate in registerList")
+      isError = true;
+    }
+    checkList.push(_registerList[i])
+  }
+  console.log("finish checking")
+  return noError
+}
+
+// REGISTER
+const register = async(_registerList) => {
   try {
-    const registerTx = await kyc.registerByList(registerList, {
+    const registerTx = await kyc.registerByList(_registerList, {
       from: kycOwnerAccount,
       gas: 4500000
     });
@@ -29,7 +72,7 @@ const register = async() => {
     //write in kyc_addresses.json
     fs.appendFileSync(path.join(__dirname, "./main_kyc_addresses.json"), JSON.stringify({
       txHash: registerTx,
-      addresses: registerList
+      addresses: _registerList
     }, undefined, 2));
 
   } catch (err) {
@@ -37,29 +80,14 @@ const register = async() => {
   }
 }
 
-//CHECK
-const check_registerList = async() => {
-  const checkList = []
-  let noError = true;
-  for (let i = 0; i < registerList.length; i++) {
-    if (await kyc.registeredAddress(registerList[i])) {
-      console.log(i, registerList[i], " is already registered before")
-      noError = false;
-    }
 
-    if (checkList.indexOf(registerList[i]) != -1) {
-      console.log(i, registerList[i], " is duplicate in registerList")
-      noError = false;
-    }
-    checkList.push(registerList[i])
-  }
-  console.log("finish checking")
-  return noError
+// EXECUTE
+if (!registerlistProcessor()) {
+  console.log(processed_registerList)
+  check_registerList(processed_registerList).then(noError => {
+    if (noError)
+      register(processed_registerList);
+    else
+      console.log("There's a problem in main_registerList")
+  })
 }
-
-check_registerList().then(noError => {
-  if (noError)
-    register();
-  else
-    console.log("There's a problem in main_registerList")
-})
